b5d6a0d6fac66efcb6cbc55ce7e90ea865349fbf
staffan.e.nilsson <staffan.e.nilsson@70f2a8e8-31cc-11de-94d6-134fa40061eb>
2010-08-04 12:42:11 +0000

    Applied Gerhards patches to get correct background colour. Inserted LGPL headers.

--- ActivityDetailsControl.cs
+++ ActivityDetailsControl.cs
@@ -1,4 +1,20 @@
-﻿
+﻿/*
+Copyright (C) 2009, 2010 Jan Ohlson
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -158,6 +174,19 @@ namespace Janohl.ST2Funbeat
             }
         }
 
+        public void ThemeChanged(ZoneFiveSoftware.Common.Visuals.ITheme visualTheme)
+        {
+            this.BackColor = visualTheme.Control;
+            foreach (TabPage t in this.TabControl.TabPages)
+            {
+                t.BackColor = visualTheme.Control;
+            }
+        }
+
+        public void UICultureChanged(System.Globalization.CultureInfo culture)
+        {
+        }
+
         private CheckState BoolToCheckState(bool Boolean)
         {
             if (Boolean)
@@ -229,6 +258,7 @@ namespace Janohl.ST2Funbeat
         {
             RefreshInfo();
         }
+
 #endif
     }
 }
